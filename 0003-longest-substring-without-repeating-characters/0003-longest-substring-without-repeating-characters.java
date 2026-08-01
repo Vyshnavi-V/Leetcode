a9ca00cd4480614645ccc7e36a4b9e3df9efd05c
Vyshnavi V <85625961+Vyshnavi-V@users.noreply.github.com>
2026-08-01 15:37:37 +0530

    Time: 41 ms (9.11%), Space: 47.9 MB (6.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -22,21 +22,3 @@ class Solution {
 
 
 
-/*//SLIDING WINDOW + HASHMAP
-        int length=s.length();
-        HashMap<Character,Integer> map = new HashMap<>();
-        int left=0;
-        int maxLen=0;
-        //Expanding the window
-        for(int right=0;right<length;right++){
-           map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
-           //Shrinking the window; If rightChar has a count > 1
-           //// Shrink the window from the LEFT until the duplicate count drops back to 1
-           while(map.get(s.charAt(right))>1){
-                //Decrement the count
-                map.put(s.charAt(left),map.get(s.charAt(left))-1);
-                left++;
-           }
-        maxLen = Math.max(maxLen,right-left+1);
-        }
-    return maxLen;*/ 
