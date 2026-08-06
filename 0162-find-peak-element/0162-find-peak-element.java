3b51674ff249f8242f4fb00881506bf64cb7ebdd
Vyshnavi V <85625961+Vyshnavi-V@users.noreply.github.com>
2026-08-06 17:15:33 +0530

    Time: 0 ms (100%), Space: 44.2 MB (38.21%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,33 +1,24 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int length=nums.length;
-        int max=0;
         int left=0;
         int right=length-1;
-        while(left<=right){
+        while(left<right){
             int mid=left+(right-left)/2;
-            if(mid>left && mid<right){
-                if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
-                    return mid;
-                }
-                else if(nums[mid-1]>nums[mid]){
-                    right=mid-1;
-                }
-                else if(nums[mid+1]>nums[mid]){
-                    left=mid+1;
-                }
+            //If the right neighbor is larger: You are on an increasing slope, so a peak must exist somewhere to the right!!
+            // Example: [1, 2, 3, 1] with left=0, right=3 -> mid pointing at index 1 (value 2).
+            // nums[mid] (2) is less than nums[mid + 1] (3), so the array is going UP.
+            if(nums[mid]<nums[mid+1]){
+                left=mid+1;
             }
+            //If the right neighbor is smaller: You are on a decreaing slope (or at a peak), meaning a peak exists at your current position or to the left 
+            // Example: [1, 2, 3, 1] with left=2, right=3 -> mid pointing at index 2 (value 3).
+            // nums[mid] (3) is NOT less than nums[mid + 1] (1), so the array is going DOWN.
             else{
-                if(nums[left]>nums[right]){
-                    return left;
-                }
-                else{
-                    return right;
-                }
+                right=mid;
             }
-
         }
-    return 0;   
+    return left;
     }
 }
 
