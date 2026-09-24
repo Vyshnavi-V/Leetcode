2ee6172fa0bc37f4a0bdfef2f39339f578ecb8ce
Vyshnavi V <85625961+Vyshnavi-V@users.noreply.github.com>
2026-09-24 16:48:12 +0530

    Time: 11 ms (90.69%), Space: 47.4 MB (28.29%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -3,16 +3,14 @@ class Solution {
         // Using StringBuilder as a stack
          StringBuilder sb = new StringBuilder();
          for(char ch:s.toCharArray()){ 
-            // Get index of last character from stringbuilder
-            int length = sb.length()-1;
             // If last character of stringbuilder is same as current character of s 
-            if(length>=0 && sb.charAt(length)==ch){
-                sb.deleteCharAt(length); // Remove the last character
+            if(sb.length()-1>=0 && sb.charAt(sb.length()-1)==ch){
+                sb.deleteCharAt(sb.length()-1); // Remove the last character
             }
             else{  
                 sb.append(ch); // Add current character at the end
             } 
-         }
+         } 
     return sb.toString(); // Convert StringBuilder to String
     }
 }
